9cfa1eced1fa2f1b6d1f402d514708219a7b04f1
heyitskaili <63416127+heyitskaili@users.noreply.github.com>
2020-10-20 12:08:30 -0400

    Finish the animations

--- dev/js/sections/pattles.js
+++ dev/js/sections/pattles.js
@@ -4,13 +4,16 @@ gsap.set("#pattles",{transformOrigin: "center"});
 
 export function PattlesAnimation(){
     var tl = gsap.timeline();
-    tl.from("#top",{delay:0.2,duration:0.3,x:-1,alpha:0});
-    tl.from("#topright",{duration:0.3,x:-1,alpha:0, color:"ffa500"});
+    tl.from("#top",{delay:0.2,duration:0.2,x:-1,alpha:0});
+    tl.from("#topright",{duration:0.2,x:-1,alpha:0, color:"ffa500"});
     tl.from("#bottomright",{duration:0.3, x:-1,alpha:0});
-    tl.from("#bottom",{duration:0.3, x:-1,alpha:0});
-    tl.from("#bottomleft",{duration:0.3, x:-1,alpha:0});
-    tl.from("#topleft",{duration:0.3, x:-1,alpha:0});
-    tl.from("#pattles",{delay:0.3,duration: 1, rotate:360,repeat:-1});
+    tl.from("#bottom",{duration:0.2, x:-1,alpha:0});
+    tl.from("#bottomleft",{duration:0.2, x:-1,alpha:0});
+    tl.from("#topleft",{duration:0.2, x:-1,alpha:0});
+    tl.from(".second",{duration:0.3, y:-20,alpha:0});
+    tl.from(".first",{duration:0.3, y:-20,alpha:0});
+    tl.from(".third",{duration:0.3, y:-20,alpha:0});
+    tl.from("#pattles",{duration: 1, rotate:360,repeat:-1});
 
     return tl;
 }
